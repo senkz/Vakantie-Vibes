d5061d9d0ae45076fe3242f4116707e69add7767
Seth Snel <sethsnel@hotmail.com>
2011-05-30 10:35:33 +0200

    reizen.java import removed

--- src/vakantievibes/client/pages/Reizen.java
+++ src/vakantievibes/client/pages/Reizen.java
@@ -6,7 +6,6 @@ import vakantievibes.client.domain.VakantieVibes;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;

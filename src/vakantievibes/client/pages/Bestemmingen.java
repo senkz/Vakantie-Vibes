7bfc02297631400b6b78b24a59453e41bfdcd87c
Seth Snel <sethsnel@hotmail.com>
2011-06-01 12:41:54 +0200

    Added bestemmingen.java

---
+++ src/vakantievibes/client/pages/Bestemmingen.java
@@ -0,0 +1,69 @@
+package vakantievibes.client.pages;
+
+import java.util.ArrayList;
+
+import vakantievibes.client.domain.Bestemming;
+import vakantievibes.client.domain.VakantieVibes;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class Bestemmingen extends VerticalPanel {
+	
+	public VerticalPanel hoofdPanel = new VerticalPanel(), reisPanel= new VerticalPanel();
+	public Button bestemming,terug;
+	public VakantieVibes vv;
+	
+	public Bestemmingen(VakantieVibes vv) {
+		this.vv = vv;
+		ArrayList<Bestemming> bestemmingen = vv.getBestemming();
+		for(Bestemming b:bestemmingen) {
+			VerticalPanel vp = new VerticalPanel();
+			vp.add(new Label(b.getInformatie()));
+			vp.setTitle(b.getTitel());
+			bestemming = new Button("Zie de reizen");
+			bestemming.addClickHandler(new MyClickHandler(b,this));
+			vp.setStyleName("reis");
+			vp.add(bestemming);
+			hoofdPanel.add(vp);
+		}
+		add(hoofdPanel);
+	}
+		
+	class MyClickHandler implements ClickHandler {
+		public Bestemming bestemming;
+		public Bestemmingen pagina;
+		
+		public MyClickHandler(Bestemming b, Bestemmingen p) {
+			bestemming=b;
+			pagina=p;
+		}
+		
+		@Override
+		public void onClick(ClickEvent event) {
+			if(bestemming==null){
+				pagina.naarHoofdPanel();
+			} else {
+				pagina.naarReis(bestemming);
+			}
+		}
+	};
+	
+	public void naarReis(Bestemming b) {
+		remove(hoofdPanel);
+		reisPanel.add(new Reizen(vv, b));
+		terug = new Button("Terug naar Bestemmingen");
+		terug.addClickHandler(new MyClickHandler(null,this));
+		reisPanel.add(terug);
+		add(reisPanel);
+	}
+	
+	public void naarHoofdPanel() {
+		remove(reisPanel);
+		reisPanel= new VerticalPanel();
+		add(hoofdPanel);
+	}
+}
